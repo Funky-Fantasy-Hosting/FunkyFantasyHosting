4db7be3d4b76f4df052259c8745f9a67dfe174d8
Dan-Rothman <der57@pitt.edu>
2022-03-22 16:07:32 -0400

    Working on trading players

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
+import team
+
 class Player:
-    def __init__(self, id: int, name: str, nflTeam: str, leagueTeam: str, injuryStatus: str, playerRankings: int, news: str, playerBio: str):
+    def __init__(self, id: int, name: str, nflTeam: str, leagueTeam: int, injuryStatus: str, playerRankings: int, news: str, playerBio: str):
         self.id = id
         self.name = name
         self.nflTeam = nflTeam
-        self.leagueTeam = leagueTeam
+        self.leagueTeam = leagueTeam #The ID of the league team
         self.leagueStatus = None
         self.fantasyMetrics = None
         self.injuryStatus = injuryStatus
@@ -19,3 +21,10 @@ class Player:
 
     def get_id(self):
         return self.id
+
+    def get_league_team(self):
+        return self.leagueTeam
+
+    def set_league_team(self, team:team.Team):
+        self.leagueTeam = team.get_id()
+        return True
--- team.py
+++ team.py
@@ -4,36 +4,53 @@ import league, matchup, player, playoffs, user
 playerListType = 'list[player.Player]'
 
 class Team:
-    def __init__(self, name: str, playerList: playerListType, owner: int, FAABBudget: int):
+    def __init__(self, id: int, name: str, playerList: playerListType, owner: int, FAABBudget: int):
+        self.id = id
         self.name = name
         self.playerList = playerList
         self.startingLineup = None
-        self.owner = owner #The ID if the user that owns this team.
+        self.owner = owner #The ID of the user that owns this team.
         self.nextMatchup = None
         self.status = None
         self.FAABBudget = FAABBudget
         self.waiverPriority = None
         self.rosterSize = None
-        self.record = dict.fromkeys(['Wins', 'Ties', 'Losses'], 0)
+        self.record = None
         self.rank = None
+
+    def get_id(self):
+        return self.id
 
     def eval_record(self, winWeight, tieWeight, lossWeight):
         return (self.record["Wins"] * winWeight + self.record["Ties"] * tieWeight + self.record["Losses"] * lossWeight)
-
 
     def add_player(self, player: player.Player):
         for plyr in self.playerList:
             if(player.get_id() == plyr.get.id()):
                 return False
         self.playerList.add(player)
-        return None
+        player.set_league_team(self)
+        return True
 
 
-    def drop_player():
-        return None
+    def drop_player(self, player:player.Player):
+        index = 0
+        for plyr in self.playerList:
+            if(player.get_id() == plyr.get.id()):
+                self.playerList.pop(index)
+                return True
+            index += 1
+        return False
 
     
-    def trade_player():
+    @staticmethod
+    def trade_player(player1: player.Player, player2: player.Player):
+        team1 = player1.get_league_team()
+        team2 = player2.get_league_team()
+
+        team1.drop_player(player1)
+        team2.drop_player(player2)
+
         return None
 
     def set_lineup():
